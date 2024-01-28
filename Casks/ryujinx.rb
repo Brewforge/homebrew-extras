c92c5a2f225db9c93bdf143f5e8ed5476082435c
runner <runner@Mac-1706417918273.local>
2024-01-28 06:07:54 +0000

    ryujinx 1.1.1141

--- Casks/ryujinx.rb
+++ Casks/ryujinx.rb
@@ -1,6 +1,6 @@
 cask "ryujinx" do
-  version "1.1.1110"
-  sha256 "208b4b4a0bdf83d6e75d86c33424d7c6759e70f16867966c3903d8269c29fa63"
+  version "1.1.1141"
+  sha256 "1dfb027ce5a3f53698983a77fa41ad9fd8303bbda4611079f952af96a9b6f4da"
 
   url "https://github.com/Ryujinx/release-channel-master/releases/download/#{version}/test-ava-ryujinx-#{version}-macos_universal.app.tar.gz",
       verified: "github.com/Ryujinx/release-channel-master/"
