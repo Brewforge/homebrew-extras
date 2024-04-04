7c23f5c667a902ee76b1de2380c0104cb0f4076c
ivaquero <msaintms@outlook.com>
2024-04-04 21:44:45 +0800

    add zotero-beta

---
+++ Casks/zotero-beta.rb
@@ -0,0 +1,33 @@
+cask "zotero-beta" do
+  version "7.0.0-beta.68,c31a40c74"
+  sha256 "263cb8dadd9a2dcbdec8ed0026ec327123626af22125e7437c3abfa40bd0c6d3"
+
+  url "https://download.zotero.org/client/beta/#{version.csv.first}%2B#{version.csv.second}/Zotero-#{version.csv.first}%2B#{version.csv.second}.dmg"
+  name "Zotero Beta"
+  desc "Collect, organize, cite, and share research sources"
+  homepage "https://www.zotero.org/"
+
+  livecheck do
+    url "https://www.zotero.org/download/client/update/0/0/Darwin/en-US/beta/Darwin%25/update.xml"
+    strategy :xml do |xml|
+      xml.get_elements("//update[@type='major']").map do |element|
+        element.attributes["displayVersion"].split("+").join(",")
+      end
+    end
+  end
+
+  auto_updates true
+  conflicts_with cask: "zotero"
+  depends_on macos: ">= :sierra"
+
+  app "Zotero.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/org.zotero.SafariExtensionApp.SafariExtension",
+    "~/Library/Application Support/Zotero",
+    "~/Library/Caches/Zotero",
+    "~/Library/Containers/org.zotero.SafariExtensionApp.SafariExtension",
+    "~/Library/Preferences/org.zotero.zotero-beta.plist",
+    "~/Library/Saved Application State/org.zotero.zotero-beta.savedState",
+  ]
+end
