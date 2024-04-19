f7044e8b5e05d8cf4c2fdf3b9b01c40faf8cb6d1
ivaquero <msaintms@outlook.com>
2024-04-20 00:07:36 +0800

    losslessswitcher 1.1.0

--- Casks/losslessswitcher.rb
+++ Casks/losslessswitcher.rb
@@ -1,8 +1,8 @@
 cask "losslessswitcher" do
-  version "1.0.0"
-  sha256 "bf5e47312da4212c178b45df4968a40e0eb5accfe072a11f82a3814dd7dc20a6"
+  version "1.1.0"
+  sha256 "8b4b8a7ae19df9c9c135a281b48a4306803b53fc98933affa450897343e6a3e2"
 
-  url "https://github.com/vincentneo/LosslessSwitcher/releases/download/#{version}/LosslessSwitcher-v1.zip"
+  url "https://github.com/vincentneo/LosslessSwitcher/releases/download/#{version}/LosslessSwitcher-#{version.major_minor}.app.zip"
   name "LosslessSwitcher"
   desc "Automated Apple Music Lossless Sample Rate Switching for Audio Devices"
   homepage "https://github.com/vincentneo/LosslessSwitcher"
