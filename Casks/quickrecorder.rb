89bfeef6fad3448cd259a53b093cb061263baaab
ivaquero <msaintms@outlook.com>
2024-04-26 07:41:46 +0800

    quickrecorder: add 1.0.6

---
+++ Casks/quickrecorder.rb
@@ -0,0 +1,16 @@
+cask "quickrecorder" do
+  version "1.0.6"
+  sha256 "cd23520b085ea23c8dec445c49b05d966a5aa1af58a9d405bf9618d165cde7cb"
+
+  url "https://github.com/lihaoyun6/QuickRecorder/releases/download/#{version}/QuickRecorder_v#{version}.dmg"
+  name "QuickRecorder"
+  desc "Lightweight and high-performance screen recorder"
+  homepage "https://github.com/lihaoyun6/QuickRecorder"
+
+  app "QuickRecorder.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.lihaoyun6.QuickRecorder",
+    "~/Library/Saved Application State/com.lihaoyun6.QuickRecorder.savedState",
+  ]
+end
