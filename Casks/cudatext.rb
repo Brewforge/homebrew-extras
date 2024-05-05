22b8cc9a04222939a7ee941a1f3b6571b1ebb82b
ivaquero <msaintms@outlook.com>
2024-05-05 17:13:10 +0800

    cudatext: update manually

--- Casks/cudatext.rb
+++ Casks/cudatext.rb
@@ -1,11 +1,11 @@
 cask "cudatext" do
   arch arm: "aarch64", intel: "amd64"
 
-  version "1.210.0.0"
-  sha256 arm:   "b07faa4dd0adf72f3404b48414a666dd0ccd336206c5764b28f67ec82f6888b2",
-         intel: "f49431d5bb658d0e3fc9bb44a01290234d0a3a1f71a7eec112fb8c84bda029cc"
+  version "1.214.0.0"
+  sha256 arm:   "0d4d90b4a8cfb07070f2105bdadedc998bd4437528760716e07a7d904c84cc12",
+         intel: "a0699470f9a796a2a7e2619585b9c58ccd4527b1f7b9483d67b30abedd81cb66"
 
-  url "https://sourceforge.net/projects/cudatext/files/latest/download/#{version}/cudatext-macos-cocoa-#{arch}-#{version}.dmg",
+  url "https://sourceforge.net/projects/cudatext/files/release/#{version}/cudatext-macos-cocoa-#{arch}-#{version}.dmg",
       verified: "downloads.sourceforge.net/projects/cudatext/"
   name "CudaText"
   desc "Text editor"
