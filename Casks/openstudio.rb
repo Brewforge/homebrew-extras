be95bf5aa87a29cd191558035e8580386e338c40
ivaquero <msaintms@outlook.com>
2024-05-19 11:36:53 +0800

    openstudio: fix url

--- Casks/openstudio.rb
+++ Casks/openstudio.rb
@@ -5,7 +5,7 @@ cask "openstudio" do
   sha256 arm:   "c61b0cb7c64e23b94ba61dd16af9663d2a6099b18faccc8a21c65eee0abf0d5a",
          intel: "bd1a37740cb9b7592872f629043404cbc589be72c8c8e55c5e6c7087cfced9a4"
 
-  url "https://github.com/NREL/OpenStudio/releases/download/v#{version.csv.first}/OpenStudio-#{version}-Darwin-#{arch}.dmg"
+  url "https://github.com/NREL/OpenStudio/releases/download/v#{version.csv.first}/OpenStudio-#{version.csv.first}+#{version.csv.second}-Darwin-#{arch}.dmg"
   name "OpenStudio"
   desc "Collection of software tools to support whole building energy modeling"
   homepage "https://github.com/NREL/OpenStudio/"
