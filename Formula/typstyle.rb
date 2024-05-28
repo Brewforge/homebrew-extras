71eadf83347ff9c848b85c622311a9ca5d1e7bb2
ivaquero <msaintms@outlook.com>
2024-05-28 08:20:04 +0800

    typstyle: update to 0.11.24

--- Formula/typstyle.rb
+++ Formula/typstyle.rb
@@ -1,7 +1,7 @@
 class Typstyle < Formula
   desc "Format your typst source code"
   homepage "https://github.com/Enter-tainer/typstyle"
-  version "0.11.23"
+  version "0.11.24"
   license "Apache-2.0"
 
   arch = Hardware::CPU.arm? ? "arm64" : "x64"
@@ -9,7 +9,7 @@ class Typstyle < Formula
   if OS.mac?
     url "https://github.com/Enter-tainer/typstyle/releases/download/v#{version}/typstyle-darwin-#{arch}"
     if Hardware::CPU.arm?
-      sha256 "318c670994045ab50b0572e0bb550b821919c56fa32df18d54b9f0f272fc2086"
+      sha256 "74624ac55481528ff4ad6257a8c9987f4a3492d3fb5f4fa720585fe6a8a17bd5"
     else
       sha256 "d166b155b80635b45b617c80dcf5984a169955bafdfb0cb03d7ae361f6705e6b"
     end
