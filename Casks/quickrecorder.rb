40360fb4f6fb0c333562d478b2789a459d63680c
ivaquero <msaintms@outlook.com>
2024-06-17 23:16:11 +0800

    quickrecorder: update to 1.3.7

--- Casks/quickrecorder.rb
+++ Casks/quickrecorder.rb
@@ -1,6 +1,6 @@
 cask "quickrecorder" do
-  version "1.3.6"
-  sha256 "3eb6fadc391bc9071e400972cde27eca4cd38a2f55694adaee030f57593a90e4"
+  version "1.3.7"
+  sha256 "40aae1d6401e5bb4dab3b0e7f6f4a769648bd248ee0bec4698aeba54af1970a9"
 
   url "https://github.com/lihaoyun6/QuickRecorder/releases/download/#{version}/QuickRecorder_v#{version}.dmg"
   name "QuickRecorder"
