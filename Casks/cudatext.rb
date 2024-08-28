24e6202ffb47759532994a8b95ce982e566594c0
Oscaner Miao <oscaner1997@gmail.com>
2024-08-28 16:53:59 +0800

    cudatext: fix livecheck

--- Casks/cudatext.rb
+++ Casks/cudatext.rb
@@ -5,29 +5,18 @@ cask "cudatext" do
   sha256 arm:   "b07faa4dd0adf72f3404b48414a666dd0ccd336206c5764b28f67ec82f6888b2",
          intel: "f49431d5bb658d0e3fc9bb44a01290234d0a3a1f71a7eec112fb8c84bda029cc"
 
-  url "https://sourceforge.net/projects/cudatext/files/latest/download?#{version}",
+  url "http://downloads.sourceforge.net/project/cudatext/release/#{version.before_comma}/cudatext-macos-cocoa-amd64-#{version.after_comma}.dmg",
       verified: "sourceforge.net/projects/cudatext/"
   name "CudaText"
   desc "Text editor"
   homepage "https://cudatext.github.io/index.html"
 
   livecheck do
-    # url "https://sourceforge.net/projects/cudatext/files/release/"
-    # regex(/title="(\d+(\.\d+){3})"/i)
-    # strategy :page_match do |page, regex|
-    #   page.scan(regex).lazy.map do |match|
-    #     new_url = "#{url}#{match[0]}/"
-    #     version_page = Homebrew::Livecheck::Strategy.page_content(new_url)
-
-    #     next if version_page[:content].blank?
-
-    #     versions = version_page[:content].scan(/(\d+(\.\d+){3})\.dmg/i).map(&:first)
-    #     next if versions.blank?
-
-    #     versions.max
-    #   end.compact_blank.first
-    # end
-    skip "skip for now"
+    url "https://sourceforge.net/projects/cudatext/best_release.json"
+    regex(/release\/([^\/]+)\/cudatext-macos-cocoa-amd64-(.+).dmg/i)
+    strategy :page_match do |page, regex|
+      JSON.parse(page)["platform_releases"]["mac"]["filename"].scan(regex).map { |match| match[0] + "," + match[1] }
+    end
   end
 
   app "CudaText.app"
