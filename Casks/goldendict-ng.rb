1bf78a3e7542ef3758ec36c619190fd6e074f627
ivaquero <msaintms@outlook.com>
2024-09-14 16:29:42 +0800

    goldendict-ng: fix livecheck

--- Casks/goldendict-ng.rb
+++ Casks/goldendict-ng.rb
@@ -5,7 +5,7 @@ cask "goldendict-ng" do
   sha256 arm:   "0d4d90b4a8cfb07070f2105bdadedc998bd4437528760716e07a7d904c84cc12",
          intel: "a0699470f9a796a2a7e2619585b9c58ccd4527b1f7b9483d67b30abedd81cb66"
 
-  url "https://github.com/xiaoyifang/goldendict-ng/releases/download/v#{version.csv.first}-#{version.csv.second}/GoldenDict-#{version.csv.first}-Qt#{version.csv.third}-#{arch}.dmg",
+  url "https://github.com/xiaoyifang/goldendict-ng/releases/download/v#{version.csv.first}-#{version.csv.second}/GoldenDict-ng-#{version.csv.first}-Qt#{version.csv.third}-macOS-#{arch}.dmg",
       verified: "github.com/xiaoyifang/goldendict-ng/"
   name "GoldenDict"
   desc "Next Generation GoldenDict"
@@ -13,7 +13,7 @@ cask "goldendict-ng" do
 
   livecheck do
     url "https://github.com/xiaoyifang/goldendict-ng/releases"
-    regex(%r{/v(\d{2}(\.\d{2}){2})-(\w+\.\h+)/.+Qt(\d+(\.\d+)+)-#{arch}\.dmg$}i)
+    regex(%r{/v(\d{2}(\.\d{1,2}){2})-(\w+\.\h+)/.+-Qt(\d+(\.\d+)+)-macOS-#{arch}\.dmg$}i)
     strategy :github_latest do |json, regex|
       json["assets"]&.map do |asset|
         match = asset["browser_download_url"]&.match(regex)
