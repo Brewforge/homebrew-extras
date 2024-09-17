013583c0cdfaa61cac11bd144fee1be7eb42195d
ivaquero <msaintms@outlook.com>
2024-09-17 12:02:40 +0800

    tinkertool: update to 10.0

--- Casks/tinkertool.rb
+++ Casks/tinkertool.rb
@@ -1,8 +1,8 @@
 cask "tinkertool" do
-  version "9.8"
+  version "10.0"
   sha256 :no_check
 
-  url "https://www.bresink.com/download3.php?PHPSESSID=92a1feeb341686c3289ed317c51048d7"
+  url "https://www.bresink.biz/download3.php?PHPSESSID=f1bjvspkef6q63s5cc9q5gse9i"
   name "TinkerTool"
   desc "Gives you access to additional system preference settings"
   homepage "https://www.bresink.com/osx/TinkerTool.html"
@@ -11,6 +11,10 @@ cask "tinkertool" do
     url "https://www.bresink.com/osx/0TinkerTool/download.php"
     regex(/(\d+(?:\.\d+)+) \(Build \d+\)/i)
   end
+
+  installer script: {
+    executable: "#{staged_path}/Contents/MacOS/TinkerTool",
+  }
 
   app "TinkerTool.app"
 
