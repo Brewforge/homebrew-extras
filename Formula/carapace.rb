c9fd2b27c61f9a62204370f10850466f06912ecf
ivaquero <msaintms@outlook.com>
2024-09-21 18:59:10 +0800

    carapace 1.0.6

--- Formula/carapace.rb
+++ Formula/carapace.rb
@@ -2,20 +2,20 @@ class Carapace < Formula
   desc "Multi-shell multi-command argument completer"
   homepage "https://github.com/carapace-sh/carapace-bin"
 
-  version "1.0.5"
+  version "1.0.6"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/carapace-sh/carapace-bin/releases/download/v#{version}/carapace-bin_darwin_arm64.tar.gz"
-    sha256 "b4092699457d92d66026c7f7bb72302cd45ace0dd00bbf49826a2b6bcb24b27c"
+    url "https://github.com/carapace-sh/carapace-bin/releases/download/v1.0.6/carapace-bin_darwin_arm64.tar.gz"
+    sha256 "592b6d9baa9d03b60b6e942434d9b10ad2ef99b1ce526921daddd11f5a1c21f8"
   elsif OS.mac? && !Hardware::CPU.arm?
-    url "https://github.com/carapace-sh/carapace-bin/releases/download/v#{version}/carapace-bin_darwin_amd64.tar.gz"
+    url "https://github.com/carapace-sh/carapace-bin/releases/download/v1.0.5/carapace-bin_darwin_amd64.tar.gz"
     sha256 "155683a4b73c2ef8edea701f21fe4a459e9cc009ca9c9115b7bcb3aa002d3f73"
   elsif !OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/carapace-sh/carapace-bin/releases/download/v#{version}/carapace-bin_linux_arm64.tar.gz"
+    url "https://github.com/carapace-sh/carapace-bin/releases/download/v1.0.5/carapace-bin_linux_arm64.tar.gz"
     sha256 "62991654a921f2282a83c13ca8c31b5a7df712d401c375f264df6235b4b50efa"
   elsif !OS.mac? && !Hardware::CPU.arm?
-    url "https://github.com/carapace-sh/carapace-bin/releases/download/v#{version}/carapace-bin_linux_amd64.tar.gz"
+    url "https://github.com/carapace-sh/carapace-bin/releases/download/v1.0.5/carapace-bin_linux_amd64.tar.gz"
     sha256 "a7441f518a40ffc86d107059699a72c7cc652a03cbc9351f9643247ac25d7b07"
   end
 
