fcbf1ab95e0c1601fdf4e327e3b91896e95372d4
ivaquero <msaintms@outlook.com>
2024-09-21 16:09:32 +0800

    tinkertool: fix audit

--- Casks/t/tinkertool.rb
+++ Casks/t/tinkertool.rb
@@ -2,13 +2,14 @@ cask "tinkertool" do
   version "10.0"
   sha256 :no_check
 
-  url "https://www.bresink.biz/download3.php?PHPSESSID=f1bjvspkef6q63s5cc9q5gse9i"
+  url "https://bresink.biz/download3.php?PHPSESSID=f1bjvspkef6q63s5cc9q5gse9i",
+      verified: "bresink.biz/"
   name "TinkerTool"
   desc "Gives you access to additional system preference settings"
-  homepage "https://www.bresink.com/osx/TinkerTool.html"
+  homepage "https://bresink.com/osx/TinkerTool.html"
 
   livecheck do
-    url "https://www.bresink.com/osx/0TinkerTool/download.php"
+    url "https://bresink.com/osx/0TinkerTool/download.php"
     regex(/(\d+(?:\.\d+)+) \(Build \d+\)/i)
   end
 
@@ -17,6 +18,8 @@ cask "tinkertool" do
   }
 
   app "TinkerTool.app"
+
+  uninstall quit: "com.bresink.system.tinkertool"
 
   zap trash: [
     "~/Library/Application Scripts/com.bresink.system.tinkertool",
