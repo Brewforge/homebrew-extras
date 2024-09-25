571755a2ea4b2342c107a70ed7838d89aa169224
ivaquero <msaintms@outlook.com>
2024-09-25 19:33:56 +0800

    formulae: fix livecheck

--- Formula/carapace.rb
+++ Formula/carapace.rb
@@ -5,16 +5,16 @@ class Carapace < Formula
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/carapace-sh/carapace-bin/releases/download/v#{version}/carapace-bin_darwin_arm64.tar.gz"
+    url "https://github.com/carapace-sh/carapace-bin/releases/download/v1.0.6/carapace-bin_darwin_arm64.tar.gz"
     sha256 "592b6d9baa9d03b60b6e942434d9b10ad2ef99b1ce526921daddd11f5a1c21f8"
   elsif OS.mac? && !Hardware::CPU.arm?
-    url "https://github.com/carapace-sh/carapace-bin/releases/download/v#{version}/carapace-bin_darwin_amd64.tar.gz"
+    url "https://github.com/carapace-sh/carapace-bin/releases/download/v1.0.6/carapace-bin_darwin_amd64.tar.gz"
     sha256 "918d48631b0dbb425be2157a0ef02d6b617de0f2ec9e2f052b3588f4b7c82302"
   elsif !OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/carapace-sh/carapace-bin/releases/download/v#{version}/carapace-bin_linux_arm64.tar.gz"
+    url "https://github.com/carapace-sh/carapace-bin/releases/download/v1.0.6/carapace-bin_linux_arm64.tar.gz"
     sha256 "d5b057e9e338eb981db79a8c5e1b87d88bfacac5397287b48fe2490b9b385819"
   elsif !OS.mac? && !Hardware::CPU.arm?
-    url "https://github.com/carapace-sh/carapace-bin/releases/download/v#{version}/carapace-bin_linux_amd64.tar.gz"
+    url "https://github.com/carapace-sh/carapace-bin/releases/download/v1.0.6/carapace-bin_linux_amd64.tar.gz"
     sha256 "27095228f793715d530fc461b5079f03ecf95c5264a0138bcb43fc6a08274f79"
   end
 
--- Formula/termscp.rb
+++ Formula/termscp.rb
@@ -1,23 +1,22 @@
 class Termscp < Formula
   desc "Feature rich terminal file transfer and explorer"
   homepage "https://termscp.veeso.dev/"
-  version "0.14.0"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/veeso/termscp/releases/latest/download/termscp-v#{version}-arm64-apple-darwin.tar.gz"
+    url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.14.0-arm64-apple-darwin.tar.gz"
     sha256 "d312dfb13c125bd18112e816f74008e234c02e410ca7b4617bb9bfbdfc4c5664"
   elsif OS.mac? && !Hardware::CPU.arm?
-    url "https://github.com/veeso/termscp/releases/latest/download/termscp-v#{version}-x86_64-apple-darwin.tar.gz"
+    url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.14.0-x86_64-apple-darwin.tar.gz"
     sha256 "63b0eea8a8ab3457d1a92d0b9c0cf492bd4ab0220a3e54db416b31b29aadc541"
   elsif !OS.mac?
     depends_on "dbus"
     depends_on "samba"
     if Hardware::CPU.arm?
-      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.14.0-aarch64-unknown-linux-gnu.tar.gz"
       sha256 "fa2620d7b4d0c29e354d47daa9c79248812e68930525c8e7b9ddcc38a12f19fd"
     else
-      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/veeso/termscp/releases/latest/download/termscp-v0.14.0-x86_64-unknown-linux-gnu.tar.gz"
       sha256 "ebee357f3a01ff356975ffe6a2bc074cf60022613237c6fc6000f57bdd2b4fa6"
     end
   end
