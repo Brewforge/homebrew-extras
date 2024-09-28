27ae4ee667ee4cb986201382b23958e196c0087c
_ivaquero_ <46377665+ivaquero@users.noreply.github.com>
2024-09-28 14:28:17 +0800

    pareto-security 1.7.87 (#236)
    Co-authored-by: Brewforge-Bot <brewforge-bot@aliyun.com>

--- Casks/p/pareto-security.rb
+++ Casks/p/pareto-security.rb
@@ -1,5 +1,5 @@
 ﻿cask "pareto-security" do
-  version "1.7.85"
+  version "1.7.87"
   sha256 :no_check
 
   url "https://github.com/ParetoSecurity/pareto-mac/releases/download/#{version}/ParetoSecurity.dmg",
