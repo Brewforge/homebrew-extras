d56ca4812b77564aff8e89730d6cfc3195c02636
ivaquero <msaintms@outlook.com>
2024-10-14 22:08:28 +0800

    corsixth: update to 0.68.0

--- Casks/c/corsixth.rb
+++ Casks/c/corsixth.rb
@@ -1,8 +1,11 @@
 cask "corsixth" do
-  version "0.67"
-  sha256 "f99a4976416763eeca683f56b56b25787e1a84751e74995675fc2e6124256e74"
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://github.com/CorsixTH/CorsixTH/releases/download/v#{version}/CorsixTH.#{version}-1.dmg"
+  version "0.68.0"
+  sha256 arm:   "60b1a94f94e0aaf2cbd2dcd865287cf2332610e474a851681c17a8b586e5ae03",
+         intel: "523c9ffa4eda0a8657000d708acd733dbb4fc094cf0804a1cf21290706c8a97e"
+
+  url "https://github.com/CorsixTH/CorsixTH/releases/download/v#{version}/CorsixTH-#{version}-#{arch}.dmg"
   name "CorsixTH"
   desc "Open source clone of Theme Hospital"
   homepage "https://github.com/CorsixTH/CorsixTH/"
@@ -14,5 +17,8 @@ cask "corsixth" do
 
   app "CorsixTH.app"
 
-  zap trash: ""
+  zap trash: [
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/C/com.macsourceports.CorsixTH",
+    "~/Library/Saved Application State/com.macsourceports.CorsixTH.savedState"
+  ]
 end
