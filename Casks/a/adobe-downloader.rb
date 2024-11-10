d2fd16e512d991c8108c045db12ff29bf7dc35b2
_ivaquero_ <46377665+ivaquero@users.noreply.github.com>
2024-11-10 14:13:05 +0800

    adobe-downloader 1.1.0 (#281)
    Co-authored-by: Brewforge-Bot <brewforge-bot@aliyun.com>

--- Casks/a/adobe-downloader.rb
+++ Casks/a/adobe-downloader.rb
@@ -1,6 +1,6 @@
 cask "adobe-downloader" do
-  version "1.0.1"
-  sha256 "8bf8da37b2e4be99f2e5bc07ca4b182e1a9cfb324b046dd31d0cc3b0e7c15f79"
+  version "1.1.0"
+  sha256 "fad24be97a30bd60afe03b337e57f65065810e3351d7368699f2b80d59360c98"
 
   url "https://github.com/X1a0He/Adobe-Downloader/releases/download/#{version}/Adobe.Downloader.dmg"
   name "Adobe Downloader"
