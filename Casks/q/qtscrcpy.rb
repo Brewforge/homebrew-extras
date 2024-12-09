c98059b7e028c3b690b67e50d7a636d0af4e446d
_ivaquero_ <46377665+ivaquero@users.noreply.github.com>
2024-12-09 14:35:07 +0800

    qtscrcpy 3.0.1 (#315)

--- Casks/q/qtscrcpy.rb
+++ Casks/q/qtscrcpy.rb
@@ -1,9 +1,9 @@
 cask "qtscrcpy" do
   arch arm: "x64", intel: "x64"
 
-  version "3.0.0"
-  sha256 arm:   "ef0c763865cb7d93960515153eb990b30a05ff94a795b9f6a36163d8bda30d59",
-         intel: "ef0c763865cb7d93960515153eb990b30a05ff94a795b9f6a36163d8bda30d59"
+  version "3.0.1"
+  sha256 arm:   "01362953ec86a0129a5c5b014c9d26d173cb015fc003eb3fc1f16717cb72f621",
+         intel: "01362953ec86a0129a5c5b014c9d26d173cb015fc003eb3fc1f16717cb72f621"
 
   url "https://github.com/barry-ran/QtScrcpy/releases/download/v#{version}/QtScrcpy-mac-#{arch}-v#{version}.dmg"
   name "QtScrcpy"
