049817a371c5a056b068d38c4ef60823e01ac952
_ivaquero_ <46377665+ivaquero@users.noreply.github.com>
2025-01-15 09:23:01 +0800

    butterfly 2.2.4 (#340)
    Co-authored-by: Brewforge-Bot <brewforge-bot@aliyun.com>

--- Casks/b/butterfly.rb
+++ Casks/b/butterfly.rb
@@ -1,6 +1,6 @@
 cask "butterfly" do
-  version "2.2.3"
-  sha256 "a86ea62d2e4329c8a5bb57375f004cdda251e76fffb8d3e58853884b0c419f7e"
+  version "2.2.4"
+  sha256 "b54712ac1582e1b8472c91f87afddb8b6aef156b7af21faaaf83fbc669e11e79"
 
   url "https://github.com/LinwoodDev/Butterfly/releases/download/v#{version}/linwood-butterfly-macos.dmg"
   name "Butterfly"
