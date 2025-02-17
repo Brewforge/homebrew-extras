af28d656e56561880ebd9322a11ca3816b8a4138
ivaquero <msaintms@outlook.com>
2025-02-17 15:21:03 +0800

    airy-ai: fix audit

--- Casks/a/airy-ai.rb
+++ Casks/a/airy-ai.rb
@@ -2,8 +2,7 @@ cask "airy-ai" do
   arch arm: "arm64", intel: "64"
 
   version "1.1.1"
-  sha256 arm:   "95986d845466153e541b2d09bf8025fa8c612771b7b47e8a70c74c6b080d1416",
-         intel: "5633656e9e78fe5590563ce59132320ef228708843f7ac368dd470f535acb8fb"
+  sha256 :no_check
 
   url "https://electron-release-cdn.codefuture.top/download/flavor/Airy/latest/osx_#{arch}",
       verified: "electron-release-cdn.codefuture.top/"
