6afb1d9c671234bb85f40f48448a513182cb8bae
_ivaquero_ <46377665+ivaquero@users.noreply.github.com>
2025-02-28 14:34:52 +0800

    pareto-security 1.8.8 (#386)

--- Casks/p/pareto-security.rb
+++ Casks/p/pareto-security.rb
@@ -1,5 +1,5 @@
 ﻿cask "pareto-security" do
-  version "1.8.4"
+  version "1.8.8"
   sha256 :no_check
 
   url "https://github.com/ParetoSecurity/pareto-mac/releases/download/#{version}/ParetoSecurity.dmg",
