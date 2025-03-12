46861701d65ce26a33dac49255a56fad1b0dd576
_ivaquero_ <46377665+ivaquero@users.noreply.github.com>
2025-03-12 14:57:47 +0800

    5ire 0.9.8 (#397)

--- Casks/#/5ire.rb
+++ Casks/#/5ire.rb
@@ -1,9 +1,9 @@
 cask "5ire" do
   arch arm: "-arm64", intel: ""
 
-  version "0.9.7"
-  sha256 arm:   "0fd86fd2a583ace7133cbb0dbed96a4896f85aae9e3f7970648d6ae362fa1b50",
-         intel: "24408d7e28beda53b9f5b64f48c28214052ba264d802979e0130238c47b2f026"
+  version "0.9.8"
+  sha256 arm:   "625f1e9427644e5a7cc605ab41c6f9e927ec14c3bd4767466751c1e853aca2de",
+         intel: "ae33427391199c136acd0f4958977a54defa6f48b66f56f7a2a2982f182c81fc"
 
   url "https://github.com/nanbingxyz/5ire/releases/download/v#{version}/5ire-#{version}#{arch}.dmg",
       verified: "github.com/nanbingxyz/5ire/"
