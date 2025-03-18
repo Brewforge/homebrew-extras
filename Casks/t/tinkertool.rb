aaf9c20fa431a198be887715e96022edac3dafad
ivaquero <msaintms@outlook.com>
2025-03-18 18:46:41 +0800

    tinkertool: installation error

--- Casks/t/tinkertool.rb
+++ Casks/t/tinkertool.rb
@@ -1,16 +1,17 @@
 cask "tinkertool" do
-  version "10.0"
+  version "10.2"
   sha256 :no_check
 
-  url "https://bresink.biz/download3.php?PHPSESSID=f1bjvspkef6q63s5cc9q5gse9i",
+  url "https://bresink.com/download3.php?PHPSESSID=8c4b0e27ab71eb830d06841116646228",
       verified: "bresink.biz/"
   name "TinkerTool"
   desc "Gives you access to additional system preference settings"
   homepage "https://bresink.com/osx/TinkerTool.html"
 
   livecheck do
-    url "https://bresink.com/osx/0TinkerTool/download.php"
-    regex(/(\d+(?:\.\d+)+) \(Build \d+\)/i)
+    # url "https://bresink.com/osx/0TinkerTool/download.php"
+    # regex(/(\d+(?:\.\d+)+) \(Build \d+\)/i)
+    skip "error"
   end
 
   app "TinkerTool.app"
