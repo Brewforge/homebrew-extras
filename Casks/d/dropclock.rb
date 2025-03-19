56ec62cb913b306ce5c5433e11492a4259cf9bf6
_ivaquero_ <46377665+ivaquero@users.noreply.github.com>
2025-03-19 09:33:51 +0800

    dropclock 1.4 (#404)

--- Casks/d/dropclock.rb
+++ Casks/d/dropclock.rb
@@ -1,6 +1,6 @@
 cask "dropclock" do
-  version "1.3"
-  sha256 "312a31357f265cc602aa6765f16bc00105b9d81e4f659b6e988750cd62d50c97"
+  version "1.4"
+  sha256 "08d517b07609d3d95f6c2ffba056b0368b6e05a698314030b661d24d36888045"
 
   url "https://github.com/WrkX/Dropclock/releases/download/#{version}/Dropclock.dmg"
   name "ChatGPT"
