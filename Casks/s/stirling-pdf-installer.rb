bed6c078129b4eb010c87f3dd90833bf175e712e
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2025-04-10 17:38:02 +0800

    stirling-pdf-installer 0.45.3 (#423)
    Co-authored-by: Brewforge-Bot <brewforge-bot@aliyun.com>

--- Casks/s/stirling-pdf-installer.rb
+++ Casks/s/stirling-pdf-installer.rb
@@ -1,6 +1,6 @@
 cask "stirling-pdf-installer" do
-  version "0.45.0"
-  sha256 "4247dfc55640c1849e583cddb8a6d63051daf169ba9e388f1c68122512035c77"
+  version "0.45.3"
+  sha256 "85c8ba6e3973897d8864525d16fbce8a08bb4a13f46a8354276c0aa980ba78a2"
 
   url "https://github.com/Stirling-Tools/Stirling-PDF/releases/download/v#{version}/Stirling-PDF-mac-installer.dmg",
       verified: "github.com/Stirling-Tools/Stirling-PDF/"
