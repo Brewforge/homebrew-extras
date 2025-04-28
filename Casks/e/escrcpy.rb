00303f1632c206ccbb0dea125369e2fa29ee5abb
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2025-04-28 18:42:20 +0800

    escrcpy 1.29.6 (#437)

--- Casks/e/escrcpy.rb
+++ Casks/e/escrcpy.rb
@@ -1,9 +1,9 @@
 cask "escrcpy" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.29.5"
-  sha256 arm:   "8c514b0455a4b2c6133487f77073e64ca44ee87228deb752368fd9c267aef5ce",
-         intel: "a90c9682ddffbb9e4a7f93d1d8d4f6b91f6e2607c1ddeb2d0232ebad63d87027"
+  version "1.29.6"
+  sha256 arm:   "1cdb7bc0b078b974f82b9cccaee279184372e4d38d2ca5c6fbb37745a6dd02b6",
+         intel: "8e5f8fdc5bb8a49fe5c55996c771443ed4e4f2903dea4464938bcfdf23c61339"
 
   url "https://github.com/viarotel-org/escrcpy/releases/download/v#{version}/Escrcpy-#{version}-mac-#{arch}.dmg"
   name "Escrcpy"
