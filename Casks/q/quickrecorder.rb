8fd5eb10ec4986e6246b0c588352e64293453db4
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2025-05-01 10:06:55 +0800

    quickrecorder 1.6.6 (#440)
    Co-authored-by: Brewforge-Bot <brewforge-bot@aliyun.com>

--- Casks/q/quickrecorder.rb
+++ Casks/q/quickrecorder.rb
@@ -1,6 +1,6 @@
 cask "quickrecorder" do
-  version "1.6.5"
-  sha256 "25e4aa84f51cb4aa7a72621d3b95479c9fd395c18d3419541e3c3f3d26389f50"
+  version "1.6.6"
+  sha256 "10023824273d039c67efb66e0ac9e90a9bff5f5d2e9820fd5139c2242dbcd129"
 
   url "https://github.com/lihaoyun6/QuickRecorder/releases/download/#{version}/QuickRecorder_v#{version}.dmg"
   name "QuickRecorder"
