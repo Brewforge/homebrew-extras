792062f2de5859b83edc21b998cb079f298a6040
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2025-06-16 16:37:18 +0800

    escrcpy 1.29.7 (#477)
    Co-authored-by: Brewforge-Bot <brewforge-bot@aliyun.com>

--- Casks/e/escrcpy.rb
+++ Casks/e/escrcpy.rb
@@ -1,9 +1,9 @@
 cask "escrcpy" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.29.6"
-  sha256 arm:   "1cdb7bc0b078b974f82b9cccaee279184372e4d38d2ca5c6fbb37745a6dd02b6",
-         intel: "8e5f8fdc5bb8a49fe5c55996c771443ed4e4f2903dea4464938bcfdf23c61339"
+  version "1.29.7"
+  sha256 arm:   "1e6e5e2b722b85c2b97cec35356e3cab4db2d302be1ff37ad733b6982560d65d",
+         intel: "95204cec2856896ffce03a95e064a02311e7615d4252fde9561e71ec2fcba713"
 
   url "https://github.com/viarotel-org/escrcpy/releases/download/v#{version}/Escrcpy-#{version}-mac-#{arch}.dmg"
   name "Escrcpy"
