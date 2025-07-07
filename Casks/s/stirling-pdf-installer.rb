e4d0eef20c662bc2aa36a977dd4c0d1ab988de5e
ivaquero <msaintms@outlook.com>
2025-07-08 07:42:34 +0800

    stirling-pdf-installer: fix url

--- Casks/s/stirling-pdf-installer.rb
+++ Casks/s/stirling-pdf-installer.rb
@@ -1,18 +1,19 @@
 cask "stirling-pdf-installer" do
-  arch arm: "", intel: "x86_64"
+  arch arm: "", intel: "-x86_64"
 
   version "0.45.6"
   sha256 arm:   "7d4a821084129e0c48be17811e7f6c3114c8b4cd605cae100b38d2d36e7a63a3",
          intel: "c0120a15ecef004302eba6262a3dfa8dbd24a4a22c3816af2070014b830a5e20"
 
-  url "https://github.com/Stirling-Tools/Stirling-PDF/releases/download/v#{version}/Stirling-PDF#{arch}-mac-installer.dmg",
+  url "https://github.com/Stirling-Tools/Stirling-PDF/releases/download/v#{version}/Stirling-PDF-mac#{arch}-installer.dmg",
       verified: "github.com/Stirling-Tools/Stirling-PDF/"
   name "Stirling PDF"
   desc "Robust, locally hosted web-based PDF manipulation tool using Docker"
   homepage "https://stirlingpdf.com/"
 
   livecheck do
-    skip "installer error"
+    url :url
+    strategy :github_latest
   end
 
   auto_updates true
