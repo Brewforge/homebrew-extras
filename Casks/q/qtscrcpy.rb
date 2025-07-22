1e1519d2eab6f1057a73fa7e866ba0108326ed4b
ivaquero <msaintms@outlook.com>
2025-07-22 14:51:32 +0800

    qtscrcpy: update

--- Casks/q/qtscrcpy.rb
+++ Casks/q/qtscrcpy.rb
@@ -1,8 +1,11 @@
 cask "qtscrcpy" do
-  version "3.3.1,6.5.3"
-  sha256 "23adbaa7e63f404a2ac91963910e7fc03e59d66b5d8a3b4c6c7addddf69fcb40"
+  arch arm: "arm64-Qt6.5.3", intel: "x64-Qt5.15.2"
 
-  url "https://github.com/barry-ran/QtScrcpy/releases/download/v#{version.csv.first}/QtScrcpy-mac-arm64-Qt#{version.csv.second}-v#{version.csv.first}.dmg"
+  version "3.3.1"
+  sha256 arm:   "23adbaa7e63f404a2ac91963910e7fc03e59d66b5d8a3b4c6c7addddf69fcb40",
+         intel: "6e24143b23511b9c33d0752f93da80bcd27a732386dc9e418dfdb495542904b4"
+
+  url "https://github.com/barry-ran/QtScrcpy/releases/download/v#{version}/QtScrcpy-mac-#{arch}-v#{version}.dmg"
   name "QtScrcpy"
   desc "Android real-time display control software"
   homepage "https://github.com/barry-ran/QtScrcpy"
