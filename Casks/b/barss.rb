98af1a52886bfbfba5ebc1c0065e79c87fb15b4d
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2025-07-22 14:47:50 +0800

    barss 1.3.1 (#502)
    Co-authored-by: Brewforge-Bot <brewforge-bot@aliyun.com>

--- Casks/b/barss.rb
+++ Casks/b/barss.rb
@@ -1,6 +1,6 @@
 cask "barss" do
-  version "1.3.0"
-  sha256 "795f139cb34cbc0a3b0db78d8094115d115951097522165b1ce49dfcff1e217b"
+  version "1.3.1"
+  sha256 "387d6a58f9f3996fb4c1d7cf732acfd24fe3a8a79992b9ea7c32c1499d77b782"
 
   url "https://github.com/relikd/baRSS/releases/download/v#{version}/baRSS_#{version}.zip",
       verified: "github.com/relikd/baRSS/"
