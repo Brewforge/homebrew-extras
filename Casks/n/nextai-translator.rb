2f43904fe77709b0ba49c533eaaaa66d3fbc71ec
ivaquero <msaintms@outlook.com>
2025-11-01 02:28:17 +0800

    nextai-translator: update to 0.6.2

--- Casks/n/nextai-translator.rb
+++ Casks/n/nextai-translator.rb
@@ -1,8 +1,9 @@
 cask "nextai-translator" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.4.34"
-  sha256 "6df834fe74f099d16947706722123c7b89e30157745037341d9caf1ee935a153"
+  version "0.6.2"
+  sha256 arm:  "6071f04a636492dd51b921bcc546ebd0ebb01f59a655a5995b6410469c853329",
+         intel: "6c3ab4b88c62779c20d205b388f2d3cff157056a3e7bd87584321cf32833841b"
 
   url "https://github.com/nextai-translator/nextai-translator/releases/download/v#{version}/NextAI.Translator_#{version}_#{arch}_darwin.dmg"
   name "OpenAI Translator"
