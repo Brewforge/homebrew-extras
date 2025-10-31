3dd07f8c9e8a2a3962f3f5525622ef39e8fca984
ivaquero <msaintms@outlook.com>
2025-11-01 02:17:33 +0800

    nextai-translator: renamed

--- Casks/o/openai-translator.rb
+++ Casks/o/openai-translator.rb
@@ -1,11 +1,11 @@
-cask "openai-translator" do
+cask "nextai-translator" do
   version "0.4.34"
   sha256 "6df834fe74f099d16947706722123c7b89e30157745037341d9caf1ee935a153"
 
-  url "https://github.com/openai-translator/openai-translator/releases/download/v#{version}/OpenAI.Translator_#{version}_aarch64.dmg"
+  url "https://github.com/nextai-translator/nextai-translator/releases/download/v#{version}/OpenAI.Translator_#{version}_aarch64.dmg"
   name "OpenAI Translator"
   desc "Cross-platform Translatior based on ChatGPT API"
-  homepage "https://github.com/openai-translator/openai-translator"
+  homepage "https://github.com/nextai-translator/nextai-translator"
 
   livecheck do
     url :url
@@ -17,10 +17,10 @@ cask "openai-translator" do
   app "OpenAI Translator.app"
 
   zap trash: [
-    "~/Library/Application Support/xyz.yetone.apps.openai-translator",
-    "~/Library/Caches/xyz.yetone.apps.openai-translator",
+    "~/Library/Application Support/xyz.yetone.apps.nextai-translator",
+    "~/Library/Caches/xyz.yetone.apps.nextai-translator",
     "~/Library/Logs/DiagnosticReports/OpenAI Translator-2024-03-02-230629.ips",
-    "~/Library/Saved Application State/xyz.yetone.apps.openai-translator.savedState",
-    "~/Library/WebKit/xyz.yetone.apps.openai-translator",
+    "~/Library/Saved Application State/xyz.yetone.apps.nextai-translator.savedState",
+    "~/Library/WebKit/xyz.yetone.apps.nextai-translator",
   ]
 end
