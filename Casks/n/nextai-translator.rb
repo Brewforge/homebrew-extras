7e52bbc60a72932dbd006379ae5860cae92f61af
ivaquero <msaintms@outlook.com>
2025-11-05 15:55:50 +0800

    nextai-translator: fix audit

--- Casks/n/nextai-translator.rb
+++ Casks/n/nextai-translator.rb
@@ -6,7 +6,7 @@ cask "nextai-translator" do
          intel: "6c3ab4b88c62779c20d205b388f2d3cff157056a3e7bd87584321cf32833841b"
 
   url "https://github.com/nextai-translator/nextai-translator/releases/download/v#{version}/NextAI.Translator_#{version}_#{arch}_darwin.dmg"
-  name "OpenAI Translator"
+  name "NextAI Translator"
   desc "Cross-platform Translatior based on ChatGPT API"
   homepage "https://github.com/nextai-translator/nextai-translator"
 
@@ -17,12 +17,14 @@ cask "nextai-translator" do
 
   auto_updates true
 
-  app "OpenAI Translator.app"
+  app "NextAI Translator.app"
 
   zap trash: [
-    "~/Library/Application Support/xyz.yetone.apps.nextai-translator",
-    "~/Library/Caches/xyz.yetone.apps.nextai-translator",
-    "~/Library/Saved Application State/xyz.yetone.apps.nextai-translator.savedState",
-    "~/Library/WebKit/xyz.yetone.apps.nextai-translator",
+    "/tmp/xyz_yetone_apps_openai_translator_si.sock",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/C/xyz.yetone.apps.openai-translator",
+    "/var/folders/py/n14256yd5r5ddms88x9bvsv40000gn/T/xyz.yetone.apps.openai-translator",
+    "~/Library/Application Support/xyz.yetone.apps.openai-translator",
+    "~/Library/Caches/xyz.yetone.apps.openai-translator",
+    "~/Library/WebKit/xyz.yetone.apps.openai-translator",
   ]
 end
