a9a8861492ea85eabc486565fcf850608445f7e2
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2025-11-18 01:07:39 +0800

    media-downloader 5.4.6 (#581)
    Co-authored-by: Brewforge-Bot <brewforge-bot@aliyun.com>

--- Casks/m/media-downloader.rb
+++ Casks/m/media-downloader.rb
@@ -1,6 +1,6 @@
 cask "media-downloader" do
-  version "5.4.5"
-  sha256 "9cc90a2449d6bbb2a897445c2baf38bb831831906d882ea6887170dadcb160e1"
+  version "5.4.6"
+  sha256 "b62cd828536f1edaf91bd40e195cfcc9b7e28d2618b080576130547d1ce1cec6"
 
   url "https://github.com/mhogomchungu/media-downloader/releases/download/#{version}/MediaDownloaderQt6-#{version}.dmg"
   name "Media Downloader"
