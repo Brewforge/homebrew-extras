766407dde209d4fd7b1e298a153d4339d7908644
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2025-11-23 17:28:12 +0800

    adobe-downloader 2.1.4 (#586)

--- Casks/a/adobe-downloader.rb
+++ Casks/a/adobe-downloader.rb
@@ -1,6 +1,6 @@
 cask "adobe-downloader" do
-  version "2.1.3"
-  sha256 "f37ca913c147e8e9f85df63281dd6eb4f42a2c007a35d85a37e53c2ade8691d2"
+  version "2.1.4"
+  sha256 "34fd86dc1d4ff3b04c8d7d6c12e9174e5e9a75904ad069c04a0e05e7fa761cac"
 
   url "https://github.com/X1a0He/Adobe-Downloader/releases/download/#{version}/Adobe.Downloader.dmg"
   name "Adobe Downloader"
