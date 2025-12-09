fe942392bfaef328e0717cbba7bcfdd55c3f24a5
ivaquero <msaintms@outlook.com>
2025-12-09 15:16:29 +0800

    tyx: fix url

--- Casks/t/tyx.rb
+++ Casks/t/tyx.rb
@@ -5,7 +5,7 @@ cask "tyx" do
   sha256 arm:   "8e02f4c9f14dbb61c171cb957151b954cdc384fd6d77ada27d06704abfc3cbc0",
          intel: "5e9c0b471878d603374e1d6ee87642af7345fd53b2cd9c6c7a1138bd6f63aed6"
 
-  url "https://github.com/tyx-editor/TyX/releases/download/v#{version}/TyX_#{version}_#{arch}_darwin.dmg",
+  url "https://github.com/tyx-editor/TyX/releases/download/v#{version}/TyX_#{version}_#{arch}.dmg",
       verified: "github.com/tyx-editor/TyX/"
   name "TyX"
   desc "LyX-like experience rewritten for Typst and the modern era"
