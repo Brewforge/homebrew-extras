c5f49d7a45f018dafb22ccec50a2398cf2dbc97a
ivaquero <msaintms@outlook.com>
2026-01-06 16:23:03 +0800

    noi: fix url

--- Casks/n/noi.rb
+++ Casks/n/noi.rb
@@ -2,7 +2,7 @@ cask "noi" do
   version "0.4.0"
   sha256 "8cfac99a877a4f9641b53afaf0dc1bc02f3eb9fb2994721ac396c84988cf0aa8"
 
-  url "https://github.com/lencx/Noi/releases/download/v#{version}/Noi_macos_#{version}-arm64.dmg"
+  url "https://github.com/lencx/Noi/releases/download/v#{version}/Noi-#{version}-arm64.dmg"
   name "Noi"
   desc "Power Your World with AI - Explore, Extend, Empower"
   homepage "https://github.com/lencx/Noi"
