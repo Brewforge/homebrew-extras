1bfe79ace485bbab20ea4f70da83299fc2a2d5bc
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2026-01-13 15:17:19 +0800

    media-downloader 5.4.7 (#638)

--- Casks/m/media-downloader.rb
+++ Casks/m/media-downloader.rb
@@ -1,6 +1,6 @@
 cask "media-downloader" do
-  version "5.4.6"
-  sha256 "b62cd828536f1edaf91bd40e195cfcc9b7e28d2618b080576130547d1ce1cec6"
+  version "5.4.7"
+  sha256 "8c389f5dc0857c85841e389710cba6292eeb09d986324be4018e9948d74a9013"
 
   url "https://github.com/mhogomchungu/media-downloader/releases/download/#{version}/MediaDownloaderQt6-#{version}.dmg"
   name "Media Downloader"
