86719482db098983e4da56530b403aa2e1694d2d
ivaquero <msaintms@outlook.com>
2026-01-15 18:12:18 +0800

    escrcpy: fix url

--- Casks/e/escrcpy.rb
+++ Casks/e/escrcpy.rb
@@ -5,7 +5,7 @@ cask "escrcpy" do
   sha256 arm:   "853f043473d616b9fcdb49958bb05a7cbc49904fb7faa9422765744ba39a91be",
          intel: "40e0c701581587d3fe548d311a56f2bd6b95d906276254b49845d3a4d69a9dd8"
 
-  url "https://github.com/viarotel-org/escrcpy/releases/download/workspace-v#{version}/Escrcpy-#{version}-mac-#{arch}.dmg"
+  url "https://github.com/viarotel-org/escrcpy/releases/download/v#{version}/Escrcpy-#{version}-mac-#{arch}.dmg"
   name "Escrcpy"
   desc "Graphical Scrcpy to display and control Android, devices powered by Electron"
   homepage "https://github.com/viarotel-org/escrcpy/"
