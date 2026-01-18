f1b02f60dd5f0517d7922c787146faf94205aed9
ivaquero <msaintms@outlook.com>
2026-01-18 17:50:09 +0800

    pixel-perfect: update to 1.5

--- Casks/p/pixel-perfect.rb
+++ Casks/p/pixel-perfect.rb
@@ -1,6 +1,6 @@
 cask "pixel-perfect" do
-  version "1.4"
-  sha256 "31b98c0d3afc71df6e4ea99be82c89d3d910ca7dbb2c6d553254e4ac86f25ece"
+  version "1.5"
+  sha256 "94c2fbb35850b3703e1677959421c761b4ad9860a8d17f383c07900fbd89c99a"
 
   url "https://github.com/cormiertyshawn895/PixelPerfect/releases/download/#{version}/PixelPerfect.#{version}.zip"
   name "Pixel Perfect"
@@ -14,17 +14,11 @@ cask "pixel-perfect" do
 
   auto_updates true
 
-  app "Pixel Perfect/Pixel Perfect.app"
+  app "Pixel Perfect.app"
 
   zap trash: [
     "~/Library/Caches/com.mac.PixelPerfect",
     "~/Library/HTTPStorages/com.mac.PixelPerfect",
     "~/Library/Preferences/com.mac.PixelPerfect.plist",
   ]
-
-  caveats <<~EOS
-    After downloading Pixel Perfect, double click to open it. macOS may prompt you “Pixel Perfect cannot be opened because it is from an unidentified developer.” This is expected.
-
-    To open Pixel Perfect, right-click on Pixel Perfect, then click “Open” as shown below.
-  EOS
 end
