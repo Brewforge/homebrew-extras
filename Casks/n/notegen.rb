58b97386881fe19ae98caa2d0a8daccbd05d3d19
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2026-01-27 23:44:18 +0800

    notegen 0.25.1 (#655)
    Co-authored-by: Brewforge-Bot <brewforge-bot@aliyun.com>

--- Casks/n/notegen.rb
+++ Casks/n/notegen.rb
@@ -1,9 +1,9 @@
 cask "notegen" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.25.0"
-  sha256 arm:   "607c3fdc0956ec246fbab1c5a2c7e8bcc1e74ff6eb27176f887035c19f9cf980",
-         intel: "6f6a9486024afad2a4da8217cf98c6abd92c682b9647b2cb84f4e003673191ae"
+  version "0.25.1"
+  sha256 arm:   "43316577695c95ad6959cc3cf8b9180d69d629cb892d34762496063583264a05",
+         intel: "894c13b840223bf216d0cfbcc176d9614a41b9cfeb475e702f5840fa14e97cd7"
 
   url "https://github.com/codexu/note-gen/releases/download/note-gen-v#{version}/NoteGen_#{version}_#{arch}.dmg",
       verified: "github.com/codexu/note-gen/"
