f168ba22367e88e2518d4ac56057207c5821aea5
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2026-02-11 15:22:45 +0800

    vibe 3.0.11 (#667)
    Co-authored-by: Brewforge-Bot <brewforge-bot@aliyun.com>

--- Casks/v/vibe.rb
+++ Casks/v/vibe.rb
@@ -1,9 +1,9 @@
 cask "vibe" do
   arch arm: "aarch64", intel: "x64"
 
-  version "3.0.9"
-  sha256 arm:   "b196d0b7e9e9e6d723af88f4167a967613519acf59e76827926a17e125ac70e9",
-         intel: "6af2d113eefa2edcea9477891ae73d417bafbba1fc1bbcf0330d09d885b8da60"
+  version "3.0.11"
+  sha256 arm:   "221ca8de480492e66d7e9c7f759e0b2d17c2c8c03094a18a98e1c6ae078c1d9b",
+         intel: "0e534658898c7f58a37116e3cc1cfeebfb9852f57ebf2260224fe16079914e43"
 
   url "https://github.com/thewh1teagle/vibe/releases/download/v#{version}/vibe_#{version}_#{arch}.dmg"
   name "vibe"
