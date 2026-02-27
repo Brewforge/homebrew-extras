d20127f4a22d1a6805bf83e64e7daa950aa9ca14
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2026-02-27 18:48:04 +0800

    notegen 0.26.0 (#680)

--- Casks/n/notegen.rb
+++ Casks/n/notegen.rb
@@ -1,9 +1,9 @@
 cask "notegen" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.25.4"
-  sha256 arm:   "8b81f817ac0716f1d46ef7c7095aafd49961b4b7b6483c706d222cbc36f979f4",
-         intel: "66a631645a33584556e04b156dd9db8761a02c0fe88c204cfd644095b0192f3b"
+  version "0.26.0"
+  sha256 arm:   "74f023fbc688fb7d518faa8853da94da3fce4cd62293dd96d87a79a793ab7403",
+         intel: "1a6a49878ca0318ed7b3db37d0dd60000c3d2d306699aba92ca324ab750e578c"
 
   url "https://github.com/codexu/note-gen/releases/download/note-gen-v#{version}/NoteGen_#{version}_#{arch}.dmg",
       verified: "github.com/codexu/note-gen/"
