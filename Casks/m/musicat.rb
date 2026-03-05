8880c25c405bacba571924ac41a585afbacb4a44
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2026-03-05 16:43:12 +0800

    musicat 0.16.0 (#685)

--- Casks/m/musicat.rb
+++ Casks/m/musicat.rb
@@ -1,9 +1,9 @@
 cask "musicat" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.15.2"
-  sha256 arm:   "a5b5d8b3bb23254dacf6f92861630ba5dad8be3247a6d83d0a7702d5433b1b1b",
-         intel: "d8040d9b5db529897270f7af5b1d56ca6491c51dd332ef461c5a655b9251bb4d"
+  version "0.16.0"
+  sha256 arm:   "effe03996f28797ffd75a9b4e20be192a4e601fdeebf5f8e0fca3854219fa2bb",
+         intel: "86fc124b6f7e75468425bc24f969836ead9e1c6b97ddbac45e09d642e9c377e4"
 
   url "https://github.com/basharovV/musicat/releases/download/v#{version}/Musicat_#{version}_#{arch}.dmg"
   name "Musicat"
