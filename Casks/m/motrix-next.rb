8a2f996ff429b230853818ee2ac455896d060510
LinxHex <92703641+LinxHex@users.noreply.github.com>
2026-03-14 23:40:07 +0800

    motrix-next 3.1.0 (#704)

---
+++ Casks/m/motrix-next.rb
@@ -0,0 +1,37 @@
+cask "motrix-next" do
+  version "3.1.0"
+  sha256 "6cd294ca0e66fe63fbf72efd58ad5496591fd91bb92bee2639d15acfb3f6de6f"
+
+  url "https://github.com/AnInsomniacy/motrix-next/releases/download/v#{version}/MotrixNext_#{version}_aarch64.dmg",
+      verified: "github.com/AnInsomniacy/motrix-next/releases/"
+  name "Motrix Next"
+  desc "Aria2-powered download manager rebuilt with Tauri"
+  homepage "https://github.com/AnInsomniacy/motrix-next"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on arch: :arm64
+  depends_on macos: ">= :big_sur"
+
+  app "MotrixNext.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.motrix.next",
+    "~/Library/Caches/com.motrix.next",
+    "~/Library/HTTPStorages/com.motrix.next",
+    "~/Library/Preferences/com.motrix.next.plist",
+    "~/Library/Saved Application State/com.motrix.next.savedState",
+  ]
+
+  caveats do
+    <<~EOS
+      Motrix Next is not code-signed. If macOS reports that it is damaged, either:
+        brew install --cask --no-quarantine aninsomniacy/motrix-next/motrix-next
+      or run:
+        xattr -cr /Applications/MotrixNext.app
+    EOS
+  end
+end
