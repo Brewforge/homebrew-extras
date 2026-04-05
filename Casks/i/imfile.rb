2c9c556a91f964c3d078952bdbd2513ca6780b2c
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2026-04-05 23:34:41 +0800

    imfile 2.0.0 (#758)

--- Casks/i/imfile.rb
+++ Casks/i/imfile.rb
@@ -1,9 +1,9 @@
 cask "imfile" do
   arch arm: "-arm64"
 
-  version "1.2.3"
-  sha256 arm:   "8203d4f341d69eeaac03130d0726713b0a0f96069912c2e0f784598d62f504c7",
-         intel: "98f620e3c47dade42097d52fc2939e2807f9715f2b95c80cb78fbe14853b829f"
+  version "2.0.0"
+  sha256 arm:   "37ccf188d05ff0537919400f34d1ce57cecb77e1a27a83f6e8baf8bc844572f8",
+         intel: "1e5434d12e10ce5e4f4a0cad55e49a707f0b1b905f5639a0908dcd23af218446"
 
   url "https://github.com/imfile-io/imfile-desktop/releases/download/v#{version}/imFile-#{version}#{arch}.dmg",
       verified: "github.com/imfile-io/imfile-desktop/"
