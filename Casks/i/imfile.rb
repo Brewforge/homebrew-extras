37ee8f9c7f9217e4cc6a3c111a27ae355d96e700
ivaquero <msaintms@outlook.com>
2026-04-05 21:09:53 +0800

    imfile: fix url

--- Casks/i/imfile.rb
+++ Casks/i/imfile.rb
@@ -5,7 +5,7 @@ cask "imfile" do
   sha256 arm:   "8203d4f341d69eeaac03130d0726713b0a0f96069912c2e0f784598d62f504c7",
          intel: "98f620e3c47dade42097d52fc2939e2807f9715f2b95c80cb78fbe14853b829f"
 
-  url "https://github.com/imfile-io/imfile-desktop/releases/download/imFile-#{version}/imFile-#{version}#{arch}.dmg",
+  url "https://github.com/imfile-io/imfile-desktop/releases/download/v#{version}/imFile-#{version}#{arch}.dmg",
       verified: "github.com/imfile-io/imfile-desktop/"
   name "imFile"
   desc "Open-source download manager"
