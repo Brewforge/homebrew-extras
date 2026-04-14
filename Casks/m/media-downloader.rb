6876924d94fd3e1fbed15b5453276716c017f37e
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2026-04-15 02:48:46 +0800

    media-downloader 5.5.1 (#793)

--- Casks/m/media-downloader.rb
+++ Casks/m/media-downloader.rb
@@ -1,9 +1,9 @@
 cask "media-downloader" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "5.5.0"
-  sha256 arm:   "6694c9107610ad453d18d2b069b2747bb81268aa6d251e8d4c52d15896039e41",
-         intel: "688f7496d655b1449818f17fa93641b91e44c4da608c6c761b3c4f8318a975c6"
+  version "5.5.1"
+  sha256 arm:   "ab5517b253b455a95859bec4658c14d61b367fb75783806909fedfeb41743629",
+         intel: "f92e0526dc4001505707fedcede79a3cf9a479df3bee1a01bacd1575df4cf656"
 
   url "https://github.com/mhogomchungu/media-downloader/releases/download/#{version}/MediaDownloaderQt6-#{arch}-#{version}.dmg"
   name "Media Downloader"
