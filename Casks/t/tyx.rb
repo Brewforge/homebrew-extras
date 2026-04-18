721b9edd7242255d8f52d4400b0aa8a21b126864
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2026-04-18 16:16:41 +0800

    tyx 0.2.18 (#802)

--- Casks/t/tyx.rb
+++ Casks/t/tyx.rb
@@ -1,9 +1,9 @@
 cask "tyx" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.2.17"
-  sha256 arm:   "8dc1bf33afbcc85b00ab78e99fdd1f48749cd7591c815f6a837285a1d332736a",
-         intel: "1f8e0f25154dfddab321474dfa081b1ab7d28198e073ca6a92bd111bc89bc646"
+  version "0.2.18"
+  sha256 arm:   "3795544330c26760f4ea144a8a4edc8aa04908ccab39cdc3d8db80cb8dd9d53f",
+         intel: "b58a945337fc7fa3a57df0b6404dc09fe10d9b6a4b83f7ec95b366d2e778229f"
 
   url "https://github.com/tyx-editor/TyX/releases/download/v#{version}/TyX_#{version}_#{arch}.dmg",
       verified: "github.com/tyx-editor/TyX/"
