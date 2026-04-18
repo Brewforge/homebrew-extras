02db3c9e4353491165d9bdf014102712fc1eabfb
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2026-04-18 16:16:26 +0800

    goldendict-ng 26.5.0 (#799)

--- Casks/g/goldendict-ng.rb
+++ Casks/g/goldendict-ng.rb
@@ -1,6 +1,6 @@
 cask "goldendict-ng" do
-  version "26.3.0,Release.fce2b872,6.9.1"
-  sha256 "7dc4cc72ef0510f5b1716b82ef5377a3719b2bfcaba1dc9b14b960f3fe67d53e"
+  version "26.5.0,Release.c5e2abc1,6.9.1"
+  sha256 "63843cf863060ad8025ccb5a47e183bd271b4ae3bbb5e843792553cb319c3300"
 
   url "https://github.com/xiaoyifang/goldendict-ng/releases/download/v#{version.csv.first}-#{version.csv.second}/GoldenDict-ng-#{version.csv.first}-Qt#{version.csv.third}-macOS-arm64.dmg",
       verified: "github.com/xiaoyifang/goldendict-ng/"
