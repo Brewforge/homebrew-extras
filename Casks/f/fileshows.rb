9310f4926e7bf22fe794be6728062179d2614e83
crown.hg <crown.hg@gmail.com>
2026-04-24 17:06:22 +0800

    fileshows and gai: add 1.0.2 and 1.2.6, respectively

---
+++ Casks/f/fileshows.rb
@@ -0,0 +1,28 @@
+# Documentation: https://docs.brew.sh/Cask-Cookbook
+#                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+cask "fileshows" do
+  version "1.0.2"
+  sha256 "37007ec49db45741841fda55038a5d1e95a2a72c695c12876e68fb2aff260bdf"
+
+  url "https://webpath.iche2.com/release/FileShows-#{version}-universal.dmg"
+  name "FileShows"
+  desc "FileShows: A file searcher with glob patterns and file streams."
+  homepage "https://webpath.iche2.com/fssdoc/en/"
+
+  # Documentation: https://docs.brew.sh/Brew-Livecheck
+  livecheck do
+    url "https://webpath.iche2.com/release/"
+    regex(/FileShows-(\d+(?:\.\d+)*)-universal\.dmg/i)
+    strategy :page_match
+  end
+
+  depends_on macos: ">= :catalina"
+
+  app "FileShows.app"
+
+  # Documentation: https://docs.brew.sh/Cask-Cookbook#stanza-zap
+  zap trash: [
+    "~/Library/Caches/com.iche2.fileshows",
+  ]
+end
---
+++ Casks/g/gai.rb
@@ -0,0 +1,28 @@
+# Documentation: https://docs.brew.sh/Cask-Cookbook
+#                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+cask "gai" do
+  version "1.2.6"
+  sha256 "34597fbfd1625afb21c017728916fc8ae0e3a0aeaa2e01095f62b2f4b8b4fe0d"
+
+  url "https://webpath.iche2.com/release/Gai-#{version}-universal.dmg"
+  name "Gai"
+  desc "Generative-AI Tools For Beginner"
+  homepage "https://webpath.iche2.com/gaidoc/en/"
+
+  # Documentation: https://docs.brew.sh/Brew-Livecheck
+  livecheck do
+    url "https://webpath.iche2.com/release/"
+    regex(/Gai-(\d+(?:\.\d+)*)-universal\.dmg/i)
+    strategy :page_match
+  end
+
+  depends_on macos: ">= :catalina"
+
+  app "Gai.app"
+
+  # Documentation: https://docs.brew.sh/Cask-Cookbook#stanza-zap
+  zap trash: [
+    "~/Library/Caches/com.iche2.gai.macos",
+  ]
+end
