aaa39232f4623269f6bbcd8dc3fb470d5a9a2f16
ivaquero <msaintms@outlook.com>
2026-05-09 02:01:47 +0800

    cytoscape: fix audit

--- Casks/c/cytoscape.rb
+++ Casks/c/cytoscape.rb
@@ -2,12 +2,20 @@ cask "cytoscape" do
   arch arm: "aarch64", intel: "x64"
 
   version "3.10.4"
-  sha256 arm:   "aacfa17a564db5993da006a05e7b661aa2eb6e8ec0d132f0c4d3af74f62e0d2c",
-         intel: "f1d898cc07ab0c74e02a559c97224e7f0fd84094e66f50f9e263c686efdae93c"
+  sha256 arm:          "aacfa17a564db5993da006a05e7b661aa2eb6e8ec0d132f0c4d3af74f62e0d2c",
+         intel:        "f1d898cc07ab0c74e02a559c97224e7f0fd84094e66f50f9e263c686efdae93c",
+         x86_64_linux: "807094f7b01f073a38afe17e15ce651ddfa442d8ff36ca02362a4086fe4eeec1"
 
-  version2 = version.tr(".", "_")
-  url "https://github.com/cytoscape/cytoscape/releases/download/#{version}/Cytoscape_#{version2}_macos_#{arch}.dmg",
-      verified: "github.com/cytoscape/cytoscape/"
+  on_macos do
+    version2 = version.tr(".", "_")
+    url "https://github.com/cytoscape/cytoscape/releases/download/#{version}/Cytoscape_#{version2}_macos_#{arch}.dmg",
+        verified: "github.com/cytoscape/cytoscape/"
+  end
+
+  on_linux do
+    url "https://github.com/cytoscape/cytoscape/releases/download/#{version}/cytoscape-unix-#{version}.tar.gz",
+        verified: "github.com/cytoscape/cytoscape/"
+  end
   name "Cytoscape"
   desc "Open source platform for network analysis and visualization"
   homepage "https://cytoscape.org/"
