9fda63a2503b080017bdfd041f07f19ac3d2ba51
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2026-07-28 04:51:11 +0800

    gridplayer: update url

--- Casks/g/gridplayer.rb
+++ Casks/g/gridplayer.rb
@@ -2,7 +2,7 @@ cask "gridplayer" do
   version "0.5.4"
   sha256 "067399c16311e3467ebedb5276b2c3c98c624c126727845570f60e1be166b6bb"
 
-  url "https://github.com/vzhd1701/gridplayer/releases/download/v#{version}/GridPlayer.#{version}.dmg"
+  url "https://github.com/vzhd1701/gridplayer/releases/download/v#{version}/GridPlayer.#{version}_#{arch}.dmg"
   name "gridplayer"
   desc "Play videos side-by-side"
   homepage "https://github.com/vzhd1701/gridplayer"
