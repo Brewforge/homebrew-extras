533ec887fefdbcf14e7489edeec04c01d038459b
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2026-07-28 04:52:57 +0800

    gridplayer: update url

--- Casks/g/gridplayer.rb
+++ Casks/g/gridplayer.rb
@@ -1,6 +1,9 @@
 cask "gridplayer" do
+  arch arm: "arm64", intel: "intel64"
+
   version "0.5.4"
-  sha256 "067399c16311e3467ebedb5276b2c3c98c624c126727845570f60e1be166b6bb"
+  sha256 arm:   "df730c93af97bad5200bd24988f2c18edb87411359db1fd9bac737ecefccd73e",
+         intel: "848823731819e21c615a3ca39c7b968b2af47bdc6df32ee7682026e2ed3422eb"
 
   url "https://github.com/vzhd1701/gridplayer/releases/download/v#{version}/GridPlayer.#{version}_#{arch}.dmg"
   name "gridplayer"
