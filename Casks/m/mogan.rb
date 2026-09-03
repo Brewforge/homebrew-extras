33bba30e1c592972e9d15c5e88ce8717858b5e66
ivaquerozz <46377665+ivaquero@users.noreply.github.com>
2026-09-04 01:25:31 +0800

    mogan: fix url

--- Casks/m/mogan.rb
+++ Casks/m/mogan.rb
@@ -2,8 +2,7 @@ cask "mogan" do
   version "2026.3.2"
   sha256 "144507514e2ae389e09da95d69647f5b9645ecf91179e50be9475e565e408e7e"
 
-  url "https://github.com/XmacsLabs/mogan/releases/download/v#{version}/MoganSTEM-v#{version}-arm.dmg",
-      verified: "github.com/XmacsLabs/mogan/releases/"
+  url "https://github.com/MoganLab/mogan/releases/download/v#{version}/mogan-release-#{version}-osx-arm64-stable.zip"
   name "Mogan STEM"
   desc "Structured STEM suite"
   homepage "https://mogan.app/"
@@ -13,7 +12,6 @@ cask "mogan" do
     strategy :github_latest
   end
 
-  depends_on arch: :arm64
   depends_on macos: :sonoma
 
   app "MoganSTEM.app"
